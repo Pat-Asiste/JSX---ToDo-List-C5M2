fcf2a6426fc43a9ce9e2771d3564207ee36d83bc
Pat-Asiste <barrantespatrick@gmail.com>
2026-01-08 21:55:38 -0500

    funcionalidad addItems FRONTEND

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import './TodoList.css';
 
 const TodoList = () => {
-  //DB2 CREADO-------------------------------
+  // DB2 CREADO-------------------------------
   const [todos, setTodos] = useState([]);//Memoria ROM
   const [headingInput, setHeadingInput] = useState("");//Memoria enTiempoReal
   const [listInputs, setListInputs] = useState({});
+  // ------------------------------------------
 
-  //FUNCIONALIDAD DE AGREGAR ITEMS
+  // FUNCIONALIDAD DE AGREGAR ITEMS 
   const handleAddTodo = () => {
     if (headingInput.trim() !== "") {
       setTodos([...todos, { heading: headingInput, lists: [] }]);//Botón "add item as obj{}"
@@ -15,7 +16,7 @@ const TodoList = () => {
     }
   };
 
-
+  // UI ---------------------------------------
   return (
     <>
       <div className="todo-container">
@@ -25,13 +26,25 @@ const TodoList = () => {
             type="text"
             className="heading-input"
             placeholder="Enter heading"
-            
+            value={headingInput}
+            onChange={(e) => { setHeadingInput(e.target.value); }}
           />
-          <button className="add-list-button">Add Heading</button>
+          <button className="add-list-button" onClick={handleAddTodo}>Add Heading</button>
         </div>
       </div>
+
+      {/* ----------- DISPLAY ITEMS in UI -----------*/}
       <div className="todo_main">
-        
+        {
+        todos.map((todo, index) => (//extrae items de la DB2 //var "todo" es volátil
+          <div key={index} className="todo-card">
+            <div className="heading_todo">
+              <h3>{todo.heading}</h3>
+              <button className="delete-button-heading" onClick={() => handleDeleteTodo(index)}>Delete Heading</button>
+            </div>
+          </div>
+        ))
+        }
       </div>
     </>
   );
